5bcb023b5b9b4b64e6bbf08372f64d9eb3a89e93
Zachary Forman <au.zachary.forman@gmail.com>
2015-02-15 10:24:27 +1030

    Fairly stock standard.

--- app/src/main/java/org/csclub/android/registration/Registration.java
+++ app/src/main/java/org/csclub/android/registration/Registration.java
@@ -1,39 +1,78 @@
 package org.csclub.android.registration;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Registration extends ActionBarActivity {
+/**
+ * Allow us to make registrations easily.
+ */
+public class Registration extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+  private static final String TAG = "Registration";
+
+  private EditText nameField;
+  private EditText emailField;
+  private EditText studentIdField;
+  private EditText memberField;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_registration);
+
+    nameField = (EditText) findViewById(R.id.name_field);
+    emailField = (EditText) findViewById(R.id.email_field);
+    studentIdField = (EditText) findViewById(R.id.id_field);
+    memberField = (EditText) findViewById(R.id.num_field);
+
+    findViewById(R.id.but_submit).setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        submit();
+      }
+
+    });
+  }
+
+  /**
+   * Adds current data to the csv.
+   */
+  private void submit() {
+    String record = makeRecord();
+    nameField.getText().clear();
+    emailField.getText().clear();
+    studentIdField.getText().clear();
+    memberField.getText().clear();
+    try {
+      File path = new File(Environment.getExternalStorageDirectory(), "members.csv");
+      FileWriter fileWriter = new FileWriter(path, true);
+      fileWriter.write(record);
+      fileWriter.close();
+      Toast.makeText(this, "Successfully added member", Toast.LENGTH_LONG).show();
+    } catch (IOException exception) {
+      Log.e(TAG, "Had a problem...", exception);
     }
+  }
 
+  /**
+   * Constructs a record from the activity's fields.
+   */
+  private String makeRecord() {
+    return String.format("%s,%s,%s,%s\n",
+        nameField.getText(),
+        emailField.getText(),
+        studentIdField.getText(),
+        memberField.getText());
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_registration, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
